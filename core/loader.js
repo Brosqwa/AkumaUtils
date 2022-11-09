7b8cd2529d8a59626b0dd75397bcc1b87a505c5d
Brosqa <75503675+Brosqwa@users.noreply.github.com>
2022-11-09 22:03:02 +0100

    Fixed the coordinates of displays not updating

--- core/loader.js
+++ core/loader.js
@@ -1,12 +1,18 @@
 const { Data } = require("./data.js");
-let config = new Data("data/config.json");
+let config = new Data("data/config.json").get();
 
 module.exports = (features) => {
+
+	ChatLib.chat(features.join(", "))
 
 	features.forEach(feature => {
 		let data = require(`../features/${feature}`);
 
+		ChatLib.chat(feature);
+
 		data.forEach(trigger => {
+
+			ChatLib.chat(JSON.stringify(trigger))
 
 			switch(trigger.type) {
 				case "none":
@@ -16,7 +22,7 @@ module.exports = (features) => {
 					break;
 				case "display":
 					const config_key = trigger.display?.config_key;
-
+					
 					if(trigger.display?.config_key) {
 						if(!config[config_key])
 							config[config_key] = {
@@ -36,17 +42,15 @@ module.exports = (features) => {
 						display.setBackground(trigger.display?.bg_type);
 					if(trigger.display?.bg_color)
 						display.setBackgroundColor(trigger.display?.bg_color);
-					
-					
-
 
 					register("tick", () => {
-						config = new Data("data/config.json");
+						config = new Data("data/config.json").get();
 						if(!config[config_key].enabled)
 							return display.setShouldRender(false);
 						let x = config[config_key].x || 0;
 						let y = config[config_key].y || 0;
 						display.setShouldRender(true);
+						display.setRenderLoc(x, y);
 
 						let lines = trigger.func();
 						for(let l = 0; l < lines.length; l++) {
@@ -54,6 +58,7 @@ module.exports = (features) => {
 						}
 					})
 					break;
+					
 			}
 
 		})

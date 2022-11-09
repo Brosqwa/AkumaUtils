309cbe0162aab87063052e280060580167f2338e
Brosqa <75503675+Brosqwa@users.noreply.github.com>
2022-11-09 22:12:38 +0100

    Created for future use

---
+++ utils/json_text_component.js
@@ -0,0 +1,52 @@
+class TextComponentJSON {
+	constructor(data) {
+		this.data = data;
+		this._chain = false;
+		this.id = -1;
+	}
+
+	parseSingleObj(t) {
+		if(typeof t == "string") return new TextComponent(t);
+
+		let com = new TextComponent(t.text);
+
+		if(t.command) com.setClick("run_command", t.command);
+		if(t.hover) com.setHoverValue(t.hover);
+
+		return com;
+	}
+
+	chain() {
+		this._chain = true;
+		return this;
+	}
+
+	setID(id) {
+		this.id = id;
+		return this;
+	}
+
+	send() {
+		if(!this._chain) {
+			return new Message(this.parseSingleObj(this.data))
+				.setChatLineId(this.id)
+				.chat();
+		}
+		
+		let msg = new Message()
+			.setChatLineId(this.id);
+	
+		let x;
+		for(x = 0; x < this.data.length; x++) {
+			let v = this.parseSingleObj(this.data[x]);
+			if(v.getText() == "") { msg.addTextComponent(this.parseSingleObj("\n\n")); continue; }
+			msg.addTextComponent(this.data[x + 1] ? v.setText(v.getText() + "\n") : v);
+		}
+
+		msg.chat();
+	}
+}
+
+module.exports = {
+	TextComponentJSON
+}

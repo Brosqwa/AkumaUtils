0136ee7d62af46abdb742515a9aa8d42e90eff7f
Brosqa <75503675+Brosqwa@users.noreply.github.com>
2022-11-09 22:01:54 +0100

    Random changes + debug
    I spent like an hour trying to figure out why some code wasn'T working, turns out, I'm stupid

--- core/data.js
+++ core/data.js
@@ -1,17 +1,15 @@
 class Data {
 	constructor(path) {
 		this.path = `./config/ChatTriggers/modules/AkumaUtils/${path}`;
-		this.object = JSON.parse(
-			FileLib.read(this.path)
-		);
-		return this.get();
 	}
 
 	get() {
-		return new Proxy(this.object, {
+		let _obj = JSON.parse(FileLib.read(this.path));
+		return new Proxy(_obj, {
 			set: (obj, key, newval) => {
 
 				obj[key] = newval;
+				//ChatLib.chat(`§2[${this._path}] §a${key} §2-> §a${newval}`);
                 FileLib.write(this.path, JSON.stringify(obj, true, 4));
 			}
 		}) 

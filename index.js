1127be5738dbd83a0cccd6e2a124aff302ee4ae5
Brosqa <75503675+Brosqwa@users.noreply.github.com>
2022-11-12 23:45:06 +0100

    Added the "key_amount_display" feature to the loader

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const features = [
 	"next_mine_upgrade_display",
 	"pickaxe_xp_display",
 	"lucky_tracker",
-	"player_miner_display"
+	"player_miner_display",
+	"key_amount_display"
 ];
 
 try {
 	require("./core/loader.js")(features);
 } catch(err) {
 	ChatLib.chat(err);
-}
+}	
 
+ChatLib.chat("§4§lAkuma§f§lUtils §8» §fAkumaUtils has been reloaded");

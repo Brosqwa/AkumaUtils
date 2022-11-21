290c7b7402b004dc9f24d7c618f866c9d38983aa
Brosqa <75503675+Brosqwa@users.noreply.github.com>
2022-11-21 17:29:41 +0100

    Added some features

--- features/player_miner_display.js
+++ features/player_miner_display.js
@@ -3,67 +3,54 @@ let data = new Data("data/player_miner_display.json").get();
 
 const utils = require("../utils/utils.js");
 
-//let frames = "⚋⚊⚏⚍⚎⚌☷☶☳☵☴☲☱☰☱☲☴☵☳☶☷⚌⚎⚍⚏⚊⚋";
-//let frames = "▄▟▐▜▀▛▌▙";
-//let frames = "▛▜▙▟"
-let frames = "▙▟▜▛"
-
-let frame = -1;
-let fcd = 20;
-let last_f = frames[0];
-
 function update(event) {
 	let message = ChatLib.getChatMessage(event);
 
 	if(message.startsWith("MINERS » ")) {
 		let rmsg = message.replace("MINERS » ", "");
-		ChatLib.chat("|" + rmsg + "|");
 		switch(rmsg) {
 			case "You spawned your Player Miner!":
 				data.active = true;
 				data.start_time = Date.now();
 				data.stop_time = 0;
+				data.full_charge = false;
 				break;
 			case "Your miner ran out of battery and was despawned.":
 			case "You despawned your Player Miner!":
 				data.active = false;
 				data.stop_time = Date.now() - data.start_time;
 				data.start_time	= 0;
-				Client.showTitle("§cMiner Despawned", `§7${utils.formatTime(data.stop_time)}`, 0, 30, 0);
-				last_f = frames[frame];
+				Client.showTitle("§cMiner Despawned", `§7${utils.formatTime(data.stop_time, 2)}`, 0, 30, 0);
 				break;
-			case "Your player miner is not fully charged":
-				ChatLib.chat("carged");
-				break;
-			default:
-				ChatLib.chat("nothing?");
+			case "Your player miner is now fully charged":
+				Client.showTitle(" ", "§aMiner Charged", 0, 30, 0);
+				data.full_charge = true;
 				break;
 		}
 	}
 }
 
 function updateDisplay() {
-	fcd++;
-	if(fcd >= 5) {
-		if(frame >= frames.length - 1)
-			frame = -1
-		frame++;
-		fcd = 0;
-	}
 	let ret = [];
 	if(data.active) {
 		ret = [
 			"§a§lPlayer Miner",
 			"    §2(Mining)",
-			`§3${frames[frame]} §b${utils.formatTime(Date.now() - data.start_time)}`
+			`§3⌛ §b${utils.formatTime(Date.now() - data.start_time, 0)}`
 		];
-	} else { 
-		ret = [
-			"§c§lPlayer Miner",
-			"  §4(Charging)",
-			`§7${last_f} §f${utils.formatTime(data.stop_time)}`
-		];
-		//ret.push(utils.centerText(`${utils.formatTime(data.stop_time)}`, ret));
+	} else {
+		if(!data.full_charge)
+			ret = [
+				"§c§lPlayer Miner",
+				"  §4(Charging)",
+				`§4§m⌛§r §f${utils.formatTime(data.stop_time, 2)}`
+			];
+		else
+			ret = [
+				"§e§lPlayer Miner",
+				"  §6(Charged)",
+				`§6⌛ §f${utils.formatTime(data.stop_time, 2)}`
+			];
 	}
 	return ret;
 }
@@ -78,7 +65,6 @@ module.exports = [
 		type: "display",
 		func: updateDisplay,
 		display: {
-			config_key: "player_miner_display",
 			bg_type: "full"
 		}
 	}

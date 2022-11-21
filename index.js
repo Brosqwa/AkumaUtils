fe7c7cce4a7006df8cb17f97645472203cb14c26
Brosqa <75503675+Brosqwa@users.noreply.github.com>
2022-11-21 17:31:39 +0100

    Small changes + commands

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-const features = [
+try {
+
+const feature_list = [
 	"rpg_ammo_display",
 	"next_mine_upgrade_display",
 	"pickaxe_xp_display",
 	"lucky_tracker",
 	"player_miner_display",
-	"key_amount_display"
+	"key_amount_display",
+	"pets_display"
 ];
 
-try {
-	require("./core/loader.js")(features);
+
+require("./core/loader.js")(feature_list);
+
 } catch(err) {
 	ChatLib.chat(err);
-}	
+}
 
-ChatLib.chat("§4§lAkuma§f§lUtils §8» §fAkumaUtils has been reloaded");
+register("command", () => {
+	let nbt = Player.getHeldItem().getNBT().toObject().tag;
+
+	ChatLib.chat(JSON.stringify(nbt, true, 4));
+
+}).setName("getnbt")
+
+register("command", (...args) => {
+	try {
+		require("./command.js").cmd(args);
+	} catch(err) {
+		ChatLib.chat(err);
+	}
+}).setName("akumautils")
+
+register("command", (...args) => {
+	try {
+		require("./command.js").cc(args);
+	} catch(err) {
+		ChatLib.chat(err);
+	}
+}).setName("akumautilsconfig")
+
+
+ChatLib.chat("§4§lAkuma§f§lUtils §7- §fAkumaUtils has been loaded");
+ChatLib.chat("§fUse §b/akumautils §fto configure");
